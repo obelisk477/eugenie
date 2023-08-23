8194037814d3af1f61a8e72d7202affc709d6a87
obelisk477 <cwbatte2@gmail.com>
2023-08-22 23:12:32 -0400

    Added conditional rendering & components for dashboard based on user type

---
+++ client/src/components/CreatorDashboard.jsx
@@ -0,0 +1,8 @@
+
+function CreatorDashboard() {
+    return (
+        <h1 className="page-title"> Creator Dashboard</h1>
+    )
+  }
+  
+  export default CreatorDashboard
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,24 @@
+import { useCurrentUserContext } from '../context/CurrentUser';
+import BrandDashboard from '../components/BrandDashboard'
+import CreatorDashboard from '../components/CreatorDashboard'
+
 function Dashboard() {
+
+  let userType = useCurrentUserContext().currentUser?.brandName ? 'brand' : 'creator'
+
+  const renderDashboard = () => {
+    if (userType === 'brand') {
+      return <BrandDashboard />
+    }
+    if (userType === 'creator') {
+      return <CreatorDashboard />
+    }
+  }
+  
   return (
-    <h1 className="page-title">Dashboard</h1>
+    <>
+      {renderDashboard()}
+    </>
   )
 }
 
