157a02962939140ebf2947a682f568e46e5714fd
obelisk477 <cwbatte2@gmail.com>
2023-08-24 20:29:08 -0400

    Made isBrand available globally

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -3,7 +3,11 @@ const jwt = require('jsonwebtoken');
 const { secret, expiration } = require('./constants');
 
 module.exports = {
-  signToken ({ email, firstName, lastName, _id }) {
+  signBrandToken ({ email, brandName, _id }) {
+    const payload = { email, brandName, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+  signCreatorToken ({ email, firstName, lastName, _id }) {
     const payload = { email, firstName, lastName, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
