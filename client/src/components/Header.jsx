3a7ad10814113f45ab52a9ba2edd5722b73a2c7b
aripandini <ariannepandini@hotmail.com>
2023-08-26 20:46:57 -0400

    changed styling of home page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,15 @@
 
 import { Link } from 'react-router-dom';
 import { useCurrentUserContext } from '../context/CurrentUser';
-import { Layout, Menu } from 'antd'
+// import { Layout, Menu } from 'antd'
+import { Layout, Menu, Button } from 'antd';
+import {
+  HomeOutlined,
+  UserOutlined,
+  LoginOutlined,
+  TeamOutlined
+} from '@ant-design/icons';
+
 
 
 
@@ -9,21 +17,11 @@ const { Header } = Layout
  function MainHeader() {
   const { isLoggedIn, logoutUser } = useCurrentUserContext();
   return (
-  <Header
-style = {{
-        
-  }}
->
+  <Header id='outerNav'>
 
 {isLoggedIn() ? (
         <>
-        <Menu 
-        style = {{
-         
-        }}
-
-          mode="horizontal"
-          theme="dark">
+        <Menu mode="horizontal" theme="light">
           <Menu.Item key='dashboard'>
             <Link to="/dashboard">Dashboard</Link>
           </Menu.Item>
@@ -32,13 +30,28 @@ style = {{
         </>
       ) : (
         <>
-        <Menu
-          mode="horizontal"
-          theme="dark">
+        {/* <Menu mode="horizontal" theme="dark">
             <Menu.Item key='login'><Link to="/login">Login</Link></Menu.Item>
             <Menu.Item key='creator-registration'><Link to="/creator-registration">Creator Signup</Link></Menu.Item>
             <Menu.Item key='brand-registration'><Link to="/brand-registration">Brand Signup</Link></Menu.Item>
-        </Menu>
+        </Menu> */}
+        <Layout className="layout">
+    <Header id='innerNav' style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <div>
+      <Link to="/"><img className='logo' src='/eugenie.png' alt='brand-image'></img></Link>
+        </div>
+      <Menu id="menuItem" theme="light" mode="horizontal" defaultSelectedKeys={['1']} style={{ marginRight: '10px' }}>
+        <Menu.Item key="home" icon={<HomeOutlined />}><Link to="/">Home</Link></Menu.Item>
+        <Menu.Item key="login" icon={<LoginOutlined />}><Link to="/login">Login</Link></Menu.Item>
+        {/* <Menu.Item key="creator-registration" icon={<UserOutlined />}><Link to="/creator-registration">Creator Signup</Link></Menu.Item> */}
+        {/* <Menu.Item key="brand-registration" icon={<TeamOutlined />}><Link to="/brand-registration">Brand Signup</Link></Menu.Item> */}
+      </Menu>
+      {/* <div>
+        <Button type="primary" style={{ marginRight: '10px' }}>Sign in</Button>
+        <Button>Sign up</Button>
+      </div> */}
+    </Header>
+  </Layout>
         </>
       )} 
  </Header>
--- client/src/components/SubHeader.jsx
+++ client/src/components/SubHeader.jsx
@@ -1,35 +1,43 @@
-import { Switch } from 'antd';
-
-const styles = {
-    image: {
-        height: 'auto',
-        maxWidth: '100%',
-        width: 'auto',
-    },
-    // switch: {
-    //     width: '200px',
-    //     height: '200px'
-    // }
-}
-
+import { Link } from 'react-router-dom';
+import {
+   Col,
+    Row,
+  Button, 
+} from 'antd';
+import {
+    UserOutlined,
+    TeamOutlined
+} from '@ant-design/icons';
 
 function SubHeader() {
     return (
         <>
-          <img style={styles.image} src='/WelcomeTag.png' alt='brand-image'></img>
-          
-          <p>Flies you away on a carpet and shows you the world. 
-            <br>
-          </br>
-          Shining, Shimmering, splendid.
-          </p>
-          
-          <Switch id='switch' checkedChildren="Creator" unCheckedChildren="Brand" defaultChecked />
-   
-      
+          <Row id="heroSection" >
+            <Col span={24} id="heroTitle">
+              <img className='subHeader' src='/WelcomeTag.png' alt='brand-image'></img>
+            </Col>
+          </Row>
+          <Row id="heroSubTitleDiv" >
+            <Col span={24} id="heroSubTitle">
+            Flies you away on a carpet and shows you the world.
+            </Col>
+            <Col span={24} id="heroSubTitle">
+            Shining, Shimmering, splendid
+            </Col>
+        </Row>
+        <Row>
+        <Link to="/creator-registration">
+        <Button id='heroButtons' key="creator-registration" type="primary" shape="round" size={"large"} icon={<UserOutlined />}>
+            Creator Signup
+          </Button>
+        </Link>
+        <Link to="/brand-registration">
+        <Button id='heroButtons' key="brand-registration" type="primary" shape="round" size={"large"} icon={<TeamOutlined />}>
+          Brand Signup
+          </Button>
+        </Link>
+        </Row>
         </>
-      
-
     )
 }
 
