3429e2e6a3a5d26c0e5cdd5fcb8a44e3d644eb4d
AipuAmeh <aipuameh.16@gmail.com>
2023-08-29 19:57:02 -0400

    fixed conflict

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -45,18 +45,6 @@ const footerData = [
 ];
 // const contentList = footerData.map(( content, index ) => content)
 
-<<<<<<< HEAD
-function generateString(contentArray) {
-  let result = "";
-  for (let i = 0; i < contentArray.length; i++) {
-    result +=  contentArray[i] + "\n" ;
-  }
-  return  result ;
-  // return contentArray.join("\n");
-}
-
-=======
->>>>>>> 1c2d8a22119ff54f05ab0a455718f0c6ff31de11
 function FooterSection() {
   return (
       <Footer style={styles.footer}>
@@ -65,25 +53,13 @@ function FooterSection() {
           dataSource={footerData}
           style={styles.list}
           renderItem={(item) => (
-<<<<<<< HEAD
-            <Row style={styles.list}>
-              <Col span={12}>
-            <List.Item >
-              <List.Item.Meta title={item.title} />
-              
-            {generateString(item.content)}
-           
-=======
             <List.Item>
               <h2 style={styles.h2}>{item.title}</h2>
               <ul style={styles.ul}>
                 {item.content.map(item => <li key={item} style={styles.li}>{item}</li>)}
               </ul>
               
->>>>>>> 1c2d8a22119ff54f05ab0a455718f0c6ff31de11
             </List.Item>
-            </Col>
-            </Row>
           )}
         />
       </Footer>
