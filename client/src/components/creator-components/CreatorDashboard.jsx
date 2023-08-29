44d7f3858a21deca8725b210d2d603db0fd2609a
obelisk477 <cwbatte2@gmail.com>
2023-08-29 19:42:53 -0400

    Built collaborative workspace page

--- client/src/components/creator-components/CreatorDashboard.jsx
+++ client/src/components/creator-components/CreatorDashboard.jsx
@@ -23,19 +23,19 @@ function CreatorDashboard() {
             <main className="dashboard">
                 <Row justify="center" align="top" style={{alignItems: 'center'}}>
                     <Col span={16} >
-                        <Content title="Content title" bordered={false} style={big_styles}  >
+                        <Content title="Content title" style={big_styles} >
                             <h2>{<Link to="/dashboard/campaign-search">Find Campaigns & Brands</Link>}</h2>
                             <p>Content content</p>
                             <p>Content content</p>
                         </Content>
                     </Col>
                     <Col span={8}>
-                        <Content title="Content title" bordered={false} style={styles}>
+                        <Content title="Content title" style={styles}>
                             <h2>{<Link to="/dashboard/active-campaigns">My Brands & Campaigns</Link>}</h2>
                             <p>Content content</p>
                             <p>Content content</p>
                         </Content>
-                        <Content title="Content title" bordered={false} style={styles}>
+                        <Content title="Content title" style={styles}>
                             <h2>{<Link to="/dashboard/chats">Chats</Link>}</h2>
                             <p>Content content</p>
                             <p>Content content</p>
