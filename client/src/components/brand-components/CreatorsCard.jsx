8b1a0656a51fadde3ba2fd01651229fd67a00f2f
Josh Wong <josdanwong@gmail.com>
2023-08-31 17:45:42 -0400

    creator search thing

--- client/src/components/brand-components/CreatorsCard.jsx
+++ client/src/components/brand-components/CreatorsCard.jsx
@@ -1,16 +1,36 @@
 
 
 import { Card, Avatar } from 'antd';
-import { UserOutlined, SoundOutlined, DollarOutlined, InstagramOutlined, YoutubeOutlined, FacebookOutlined} from '@ant-design/icons';
+import { UserOutlined, TwitterOutlined, SoundOutlined, DollarOutlined, InstagramOutlined, YoutubeOutlined, FacebookOutlined} from '@ant-design/icons';
 function CreatorsCard(){
   
     return(
     <>
-    <Card style={{ width: 300, marginTop: 100 }}>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+    <Card style={{ width: 300, marginTop: 50 }}>
       <div style={{ display: 'flex', alignItems: 'center', marginBottom: 16 }}>
         <Avatar size={64} icon={<UserOutlined />} />
         <div style={{ marginLeft: 16 }}>
-          <h2>User's Name</h2>
+          <h2>Captain America</h2>
+        </div>
+      </div>
+      <div>
+        <h3>Platforms</h3>
+        <InstagramOutlined style={{ fontSize: 24, marginRight: 10 }} />
+        <YoutubeOutlined style={{ fontSize: 24, marginRight: 10 }} />
+        <TwitterOutlined style={{ fontSize: 24, marginRight: 10 }} />
+      </div>
+      <div style={{ marginTop: 16 }}>
+        <h3>Audience</h3>
+        <Avatar size={40} icon={<SoundOutlined />} />
+        <span style={{ marginLeft: 10 }}>950K</span>
+      </div>
+    </Card>
+    <Card style={{ width: 300, marginTop: 50 }}>
+      <div style={{ display: 'flex', alignItems: 'center', marginBottom: 16 }}>
+        <Avatar size={64} icon={<UserOutlined />} />
+        <div style={{ marginLeft: 16 }}>
+          <h2>Captain Marvel</h2>
         </div>
       </div>
       <div>
@@ -18,19 +38,55 @@ function CreatorsCard(){
         <InstagramOutlined style={{ fontSize: 24, marginRight: 10 }} />
         <YoutubeOutlined style={{ fontSize: 24, marginRight: 10 }} />
         <FacebookOutlined style={{ fontSize: 24, marginRight: 10 }} />
+
       </div>
       <div style={{ marginTop: 16 }}>
         <h3>Audience</h3>
         <Avatar size={40} icon={<SoundOutlined />} />
-        <span style={{ marginLeft: 10 }}>400k</span>
-      </div>
-      <div style={{ marginTop: 16 }}>
-        <h3>Cost/Campaign</h3>
-        <DollarOutlined style={{ fontSize: 24, marginRight: 10 }} />
-     <span> $40-60K </span>
-      
+        <span style={{ marginLeft: 10 }}>960K</span>
       </div>
     </Card>
+    <Card style={{ width: 300, marginTop: 50 }}>
+      <div style={{ display: 'flex', alignItems: 'center', marginBottom: 16 }}>
+        <Avatar size={64} icon={<UserOutlined />} />
+        <div style={{ marginLeft: 16 }}>
+          <h2>Mia Ramos</h2>
+        </div>
+      </div>
+      <div>
+        <h3>Platforms</h3>
+        <InstagramOutlined style={{ fontSize: 24, marginRight: 10 }} />
+        <YoutubeOutlined style={{ fontSize: 24, marginRight: 10 }} />
+        <TwitterOutlined style={{ fontSize: 24, marginRight: 10 }} />
+
+      </div>
+      <div style={{ marginTop: 16 }}>
+        <h3>Audience</h3>
+        <Avatar size={40} icon={<SoundOutlined />} />
+        <span style={{ marginLeft: 10 }}>400K</span>
+      </div>
+    </Card>
+    <Card style={{ width: 300, marginTop: 50 }}>
+      <div style={{ display: 'flex', alignItems: 'center', marginBottom: 16 }}>
+        <Avatar size={64} icon={<UserOutlined />} />
+        <div style={{ marginLeft: 16 }}>
+          <h2>Jack Sparrow</h2>
+        </div>
+      </div>
+      <div>
+        <h3>Platforms</h3>
+        <InstagramOutlined style={{ fontSize: 24, marginRight: 10 }} />
+        <YoutubeOutlined style={{ fontSize: 24, marginRight: 10 }} />
+        <FacebookOutlined style={{ fontSize: 24, marginRight: 10 }} />
+
+      </div>
+      <div style={{ marginTop: 16 }}>
+        <h3>Audience</h3>
+        <Avatar size={40} icon={<SoundOutlined />} />
+        <span style={{ marginLeft: 10 }}>740K</span>
+      </div>
+    </Card>
+    </div>
   </>
   )
 }
