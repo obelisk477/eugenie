5cea4782641cd7fedae0bc25bca56aba4b4dd7c4
Josh Wong <josdanwong@gmail.com>
2023-08-30 23:14:02 -0400

    styling check icon box alignment stuff

--- client/src/components/creator-components/CreatorRegistrationForm.jsx
+++ client/src/components/creator-components/CreatorRegistrationForm.jsx
@@ -2,10 +2,20 @@ import { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { Link, useNavigate } from "react-router-dom";
 
+import "./../../App.css";
+
 import { REGISTER_CREATOR } from "../../graphql/mutations";
 
 import { useCurrentUserContext } from "../../context/CurrentUser";
 import { Checkbox } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faYoutube,
+  faFacebook,
+  faTiktok,
+  faSnapchatGhost,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Registration() {
   const { loginUser } = useCurrentUserContext();
@@ -133,41 +143,56 @@ export default function Registration() {
           />
         </label>
         <label htmlFor="platforms">Platforms:</label>
-        <Checkbox
-          name="instagram"
-          checked={formState.platforms.instagram}
-          onChange={handleChange}
-        >
-          Instagram
-        </Checkbox>
-        <Checkbox
-          name="youtube"
-          checked={formState.platforms.youtube}
-          onChange={handleChange}
-        >
-          YouTube
-        </Checkbox>
-        <Checkbox
-          name="facebook"
-          checked={formState.platforms.facebook}
-          onChange={handleChange}
-        >
-          Facebook
-        </Checkbox>
-        <Checkbox
-          name="tiktok"
-          checked={formState.platforms.tiktok}
-          onChange={handleChange}
-        >
-          TikTok
-        </Checkbox>
-        <Checkbox
-          name="snapchat"
-          checked={formState.platforms.snapchat}
-          onChange={handleChange}
-        >
-          Snapchat
-        </Checkbox>
+        <div className="checkbox-container">
+          <label className="checkbox-label">
+            <Checkbox
+              name="instagram"
+              checked={formState.platforms.instagram}
+              onChange={handleChange}
+            />
+            <FontAwesomeIcon icon={faInstagram} className="checkbox-icon" />
+            Instagram
+          </label>
+          <label className="checkbox-label">
+            <Checkbox
+              name="youtube"
+              checked={formState.platforms.youtube}
+              onChange={handleChange}
+            />
+            <FontAwesomeIcon icon={faYoutube} className="checkbox-icon" />
+            YouTube
+          </label>
+          <label className="checkbox-label">
+            <Checkbox
+              name="facebook"
+              checked={formState.platforms.facebook}
+              onChange={handleChange}
+            />
+            <FontAwesomeIcon icon={faFacebook} className="checkbox-icon" />
+            Facebook
+          </label>
+          <label className="checkbox-label">
+            <Checkbox
+              name="tiktok"
+              checked={formState.platforms.tiktok}
+              onChange={handleChange}
+            />
+            <FontAwesomeIcon icon={faTiktok} className="checkbox-icon" />
+            TikTok
+          </label>
+          <label className="checkbox-label">
+            <Checkbox
+              name="snapchat"
+              checked={formState.platforms.snapchat}
+              onChange={handleChange}
+            />
+            <FontAwesomeIcon
+              icon={faSnapchatGhost}
+              className="checkbox-icon"
+            />
+            Snapchat
+          </label>
+        </div>
         <button type="submit">Sign Up</button>
         <p>
           Already have an account? Login <Link to="/register">here</Link>
